092fe6124d1bd0daa65d6117a944c902a95c1df9
Rohit Baghel <rohitbaghel16890@gmail.com>
2023-11-24 21:04:22 +0530

    Git commit

--- employee_details/employee_details/models.py
+++
@@ -1,9 +0,0 @@
-# myapp/models.py
-from django.db import models
-
-class Employees(models.Model):
-    emp_id = models.IntegerField()
-    name = models.CharField(max_length=300)
-    salary = models.IntegerField()
-    designation = models.CharField(max_length=200)
-    address = models.CharField(max_length=400)
